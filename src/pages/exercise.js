2e51f91987b613c190c2742b762342206cb7abeb
YO <0820967@student.osceolaschools.net>
2018-12-11 11:24:47 -0500

    excercise page beginning

--- src/pages/exercise.js
+++ src/pages/exercise.js
@@ -2,5 +2,48 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class Workout extends Component {
-  
+  constructor() {
+    super()
+    this.state = {
+      timer: 12 * 60,
+      runningTimer: 12*60,
+      running: false
+    }
+    this.workouts = {
+      Tuesday: [1,2,3,4]
+    }
+  }
+  startTimer() {
+
+  }
+  resetTimer() {
+
+  }
+  changeTimer() {
+
+  }
+  render() {
+    return (
+      <div classname="wrap page">
+        <div classname="timer"></div>
+        {
+          this.workouts[this.props.day].map(i => {
+            return <div>{i}</div>
+          })
+        }
+        <button>Next Workout</button>
+      </div>
+    )
+  }
 }
+
+function quarterChange(c) {
+  return dispatch => {
+    dispatch({
+      type: qt,
+      payload: c+1
+    })
+  }
+}
+
+export default connect(state => state, { quarterChange })(Workout)
