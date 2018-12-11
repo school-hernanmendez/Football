115c254d57ca5c6c6dedaa95fa6ba17507ca8652
YO <0820967@student.osceolaschools.net>
2018-12-11 11:11:42 -0500

    Conditional

--- src/pages/selectQ.js
+++ src/pages/selectQ.js
@@ -13,15 +13,35 @@ class Quarter extends Component {
     this.props.selectQuarter(n, this.props.history.push)
   }
   render() {
+    var workoutDay = true;
+    switch(this.props.day) {
+      case 'Friday':
+        workoutDay = false;
+        break
+      case 'Saturday':
+        workoutDay = false;
+        break
+      case 'Sunday':
+        workoutDay = false;
+        break
+      default:
+        break
+    }
     return (
       <div className="wrap page">
         <Navbar />
         <p>Today is: {this.props.day}</p>
-        <button className="quarter" onClick={() => this.select(1)}>1</button>
-        <button className="quarter" onClick={() => this.select(2)}>2</button>
-        <button className="quarter" onClick={() => this.select(3)}>3</button>
-        <button className="quarter" onClick={() => this.select(4)}>4</button>
-        <hr></hr>
+        {
+          workoutDay ? (
+          <div classname="page">
+          <button className="quarter" onClick={() => this.select(1)}>1</button>
+          <button className="quarter" onClick={() => this.select(2)}>2</button>
+          <button className="quarter" onClick={() => this.select(3)}>3</button>
+          <button className="quarter" onClick={() => this.select(4)}>4</button>
+          <hr></hr>
+          </div>
+          ) : ( <p>You shouldn't be in football class today but you can still choose a custom workout</p> )
+        }
       </div>
     )
   }
