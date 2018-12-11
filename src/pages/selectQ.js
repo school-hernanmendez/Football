0313d805e6d2ee79fddfe42a5e9527ebaf800cd4
YO <0820967@student.osceolaschools.net>
2018-12-11 11:17:12 -0500

    fixing it

--- src/pages/selectQ.js
+++ src/pages/selectQ.js
@@ -14,7 +14,7 @@ class Quarter extends Component {
   }
   render() {
     var workoutDay = true;
-    switch(this.props.day) {
+    switch (this.props.day) {
       case 'Friday':
         workoutDay = false;
         break
@@ -27,21 +27,24 @@ class Quarter extends Component {
       default:
         break
     }
-    return (
-      <div className="wrap page">
-        <Navbar />
-        <p>Today is: {this.props.day}</p>
-        {
-          workoutDay ? (
-          <div classname="page">
+    if (workoutDay) {
+      return (
+        <div className="wrap page">
+          <Navbar />
+          <p>Today is: {this.props.day}</p>
           <button className="quarter" onClick={() => this.select(1)}>1</button>
           <button className="quarter" onClick={() => this.select(2)}>2</button>
           <button className="quarter" onClick={() => this.select(3)}>3</button>
           <button className="quarter" onClick={() => this.select(4)}>4</button>
           <hr></hr>
-          </div>
-          ) : ( <p>You shouldn't be in football class today but you can still choose a custom workout</p> )
-        }
+        </div>
+      )
+    }
+    return (
+      <div className="wrap page">
+        <Navbar />
+        <p>Today is: {this.props.day}</p>
+        <p>You shouldn't be in football class today but you can still choose a custom workout</p>
       </div>
     )
   }
