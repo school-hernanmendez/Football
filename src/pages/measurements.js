d773e6d75eedc6923e5e584b5bb450ffb1045c53
YO <0820967@student.osceolaschools.net>
2018-12-13 11:24:13 -0500

    joy

--- src/pages/measurements.js
+++ src/pages/measurements.js
@@ -4,52 +4,52 @@ import { connect } from 'react-redux'
 import Navbar from './navbar.js'
 
 class Measures extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            height: props.height,
-            weight: props.weight,
-            workouts: props.workouts
+  constructor(props) {
+    super(props)
+    this.state = {
+      height: props.height,
+      weight: props.weight,
+      workouts: props.workouts
+    }
+  }
+
+  changeHeight() {
+
+  }
+
+  changeWeight() {
+
+  }
+
+  changeWorkout() {
+
+  }
+
+  submit() {
+
+  }
+  
+  render() {
+    return (
+      <div class="wrap page">
+        <Navbar />
+        <p>Set your max</p>
+        {
+          Object.keys(this.state.workouts).filter(i => {
+            return this.state.workouts[i].type != 'no'
+          }).map(i => {
+            return (
+              <div className="workout-input">
+                <p>{i.replace(/\_/g, ' ').replace(/^(.)|\s(.)/g, ($1) => $1.toUpperCase())}</p>
+                <input className="input" value={this.state.workouts[i].num}/>
+              </div>
+            )
+          })
         }
-    }
-
-    changeHeight() {
-
-    }
-
-    changeWeight() {
-
-    }
-
-    changeWorkout() {
-
-    }
-
-    submit() {
-
-    }
-    
-    render() {
-        return (
-            <div class="wrap page">
-                <Navbar />
-                <p>Set your max</p>
-                {
-                    Object.keys(this.state.workouts).filter(i => {
-                        return this.state.workouts[i].type != 'no'
-                    }).map(i => {
-                        return (
-                            <div>
-                                <p>{i.replace(/\_/g, ' ').replace(/^(.)|\s(.)/g, ($1) => $1.toUpperCase())}</p>
-                                <input type="input" value={this.state.value}/>
-                            </div>
-                        )
-                    })
-                }
-                <button>Done</button>
-            </div>
-        )
-    }
+        <button>Done</button>
+      </div>
+    )
+  }
 }
 
 function submit() {}
